7578ccea02ccb05fb96224ccb36793a98dcf077c
Christine Zosche <christine.zosche@gmail.com>
2020-05-04 20:28:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,8 +32,7 @@ class Song
     genre_hash = {}
     @@genres.each do |genre|
       if genre_hash[genre]
-          genre_hash[genre] += 1
-        end
+        genre_hash[genre] += 1
       else genre_hash[genre] = 1
       end
     end
